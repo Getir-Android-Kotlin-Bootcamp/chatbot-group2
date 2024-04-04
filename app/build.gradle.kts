d3750d3c66dda01538a3aa89b60a826343ad1412
Emre Bağdadioğlu <bagdadiogluemre@gmail.com>
2024-04-04 17:26:33 +0300

    added Retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Retrofit dependencies
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }
